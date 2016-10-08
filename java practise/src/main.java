759ec258ea843b5fb1db18fe8c9ee2aa4baaf6c8
sai2194 <harish02061995@gmail.com>
2016-10-08 17:24:28 +0530

    Inial commit

---
+++ java practise/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		int x = 8;
+		int y = 4;
+		System.out.print(x+y);
+}
+
+}
---
+++ java practise/src/sumoftwo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sumoftwo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String s = scan.next();
+
+        System.out.println(a+5);
+        System.out.println(s);
+ 
+  }
+}
