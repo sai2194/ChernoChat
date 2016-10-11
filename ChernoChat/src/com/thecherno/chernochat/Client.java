93086f1907758a3c29a14a2bf4dd5821d3f071b2
sai2194 <harish02061995@gmail.com>
2016-10-12 00:40:17 +0530

    upto 6

--- ChernoChat/src/com/thecherno/chernochat/Client.java
+++ ChernoChat/src/com/thecherno/chernochat/Client.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
 
 public class Client extends JFrame {
 
@@ -27,12 +30,26 @@ public class Client extends JFrame {
 	private void createWindow()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(850,550);
+		setSize(880,550);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{20,855,5}; //sum = 880
+		gbl_contentPane.rowHeights = new int[]{35,475,40};  // sum = 550
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea txtrHistory = new JTextArea();
+		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
+		gbc_txtrHistory.gridx = 1;
+		gbc_txtrHistory.gridy = 1;
+		contentPane.add(txtrHistory, gbc_txtrHistory);
+		
 		setVisible(true);
 	}
 
