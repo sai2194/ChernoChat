971c2e339bb7df25855c1f5fcda1fb1ae599526a
sai2194 <harish02061995@gmail.com>
2016-10-11 15:31:55 +0530

    First commit

--- ChernoChat/src/Start.java
+++ ChernoChat/src/Start.java
@@ -3,6 +3,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lets start Cherno !!!");
+		System.out.println("First Change !!");
+		
 
 	}
 
