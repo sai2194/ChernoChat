bfa97d2bf7f36726951aae442fb70b624f697810
sai2194 <harish02061995@gmail.com>
2016-10-11 14:16:29 +0530

    Initial

---
+++ chernochat/src/chernochat/Start.java
@@ -0,0 +1,11 @@
+package chernochat;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lets start ChernoChat !!!");
+
+	}
+
+}
