45a67af29581b863302b3b11e604e81d671ab239
sai2194 <harish02061995@gmail.com>
2016-10-12 10:47:29 +0530

    upto 7

--- ChernoChat/src/com/thecherno/chernochat/Client.java
+++ ChernoChat/src/com/thecherno/chernochat/Client.java
@@ -9,6 +9,9 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridBagLayout;
 import javax.swing.JTextArea;
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
 
 public class Client extends JFrame {
 
@@ -17,6 +20,7 @@ public class Client extends JFrame {
 
 	private String name,address;
 	private int port;
+	private JTextField txtMessage;
 	public Client(String name,String address,int port) {
 		
 		setTitle("Cherno Chat Client");
@@ -29,7 +33,16 @@ public class Client extends JFrame {
 	}
 	private void createWindow()
 	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		/*try {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} */
+		
+		
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(880,550);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
@@ -37,19 +50,41 @@ public class Client extends JFrame {
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{20,855,5}; //sum = 880
-		gbl_contentPane.rowHeights = new int[]{35,475,40};  // sum = 550
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{35,812,30,3}; //sum = 880
+		gbl_contentPane.rowHeights = new int[]{60,450,45};  // sum = 550
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JTextArea txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
 		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.insets = new Insets(0, 0, 5, 5);
 		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
 		gbc_txtrHistory.gridx = 1;
 		gbc_txtrHistory.gridy = 1;
+		gbc_txtrHistory.gridwidth = 2;
+		//gbc_txtrHistory.insets = new Insets(0,5,0,0);
 		contentPane.add(txtrHistory, gbc_txtrHistory);
 		
+		txtMessage = new JTextField();
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("send");
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+		
+		requestFocus();
+		txtMessage.requestFocus();
 		setVisible(true);
 	}
 
